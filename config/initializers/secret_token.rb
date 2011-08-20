c416bf454d6a4787cc7f9b0a17cf6cefa3dc8957
Eric Meyer <emeyer@8thlight.com>
2011-08-20 09:53:28 -0500

    refinery

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Owc2011::Application.config.secret_token = '3aed42b2e13549abc414ce56b7da55f2bbd7cda62ab43729835e5d8e03713889316f3978244b867c1aea46708782264323a72113efd058b7ab18383121c6dc92'
+Owc2011::Application.config.secret_token = 'e71f8efca2a5049265e6cde898821a4a55236c1ad4766ba91a8730476b093083183f84e13c70671a0b1eb1a1306a0bb1437887818b54b5256fd7a99c1327efba'
