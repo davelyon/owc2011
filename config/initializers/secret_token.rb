9dfdcc3ddf55b691140771e6a85d8780a77b168f
Eric Meyer <emeyer@8thlight.com>
2011-08-20 09:32:29 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Owc2011::Application.config.secret_token = '3aed42b2e13549abc414ce56b7da55f2bbd7cda62ab43729835e5d8e03713889316f3978244b867c1aea46708782264323a72113efd058b7ab18383121c6dc92'
