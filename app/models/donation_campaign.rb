3bfe93b7e1a3e87bb85f2d6ac44ac6e7f9359170
Paul Pagel <paul@8thlight.com>
2011-08-21 02:04:12 -0500

    now sorts donation campaigns from most expensive to least

--- app/models/donation_campaign.rb
+++ app/models/donation_campaign.rb
@@ -1,2 +1,5 @@
 class DonationCampaign < ActiveRecord::Base
+  def self.by_amount
+    self.all.sort { |a, b| b.amount <=> a.amount }
+  end
 end
--- spec/models/donation_campaign_spec.rb
+++ spec/models/donation_campaign_spec.rb
@@ -1,4 +1,12 @@
 require 'spec_helper'
 
 describe DonationCampaign do
+  
+  it "finds in order of expense" do
+    expensive = DonationCampaign.create!(:title => "expensive", :amount => 1000)
+    moderate = DonationCampaign.create!(:title => "expensive", :amount => 10)
+    cheap = DonationCampaign.create!(:title => "expensive", :amount => 1)
+    
+    DonationCampaign.by_amount.should == [expensive, moderate, cheap]
+  end
 end
