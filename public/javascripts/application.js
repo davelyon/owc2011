308e2dac954b7008618a02446d84669bea83b3a9
Paul Pagel <paul@8thlight.com>
2011-08-20 21:51:20 -0500

    adding character countdown

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
+
+
+
+$(document).ready(function() {
+  $("#post_tweet_twitter_post").keyup(function() {
+    var current = $("#post_tweet_twitter_post").attr("value");
+    var number_left =  140 - current.length;    
+    $("#twitter_character_coundown").html(number_left);
+  });
+});
