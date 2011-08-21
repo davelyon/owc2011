1ec291b82325f9c85a80df9c3f09eecbc1fa62f8
Brian and David <brian@8thlight.com>
2011-08-21 02:10:49 -0500

    ZUCKERBURRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRG!!!!!!!!!!

--- app/controllers/facebook_accounts_controller.rb
+++ app/controllers/facebook_accounts_controller.rb
@@ -10,7 +10,19 @@ class FacebookAccountsController < ApplicationController
     elsif params[:code] && !params[:code].empty?
       facebook_account = FacebookAccount.find(params[:id])
       facebook_account.validate_oauth_token(params[:code], facebook_callback_url(:id => facebook_account.id))
-      redirect_to(:root, :notice => 'Facebook account activated!')
+      redirect_to facebook_pages_path(params[:id])
     end
+  end
+
+  def pages
+    @account = FacebookAccount.find(params[:id])
+    @pages = @account.get_pages
+    render :template => "facebook_accounts/pages"
+  end
+
+  def assign_page
+    @account = FacebookAccount.find(params[:id])
+    @account.assign_page(params[:page_id], params[:token])
+    redirect_to(:root, :notice => 'Facebook page... selected!')
   end
 end
--- app/models/facebook_account.rb
+++ app/models/facebook_account.rb
@@ -4,14 +4,14 @@ class FacebookAccount < ActiveRecord::Base
 
   def authorize_url(callback_url = '')
     if self.oauth_authorize_url.blank?
-      self.oauth_authorize_url = "https://graph.facebook.com/oauth/authorize?client_id=#{CLIENT_ID}&redirect_uri=#{callback_url}&scope=offline_access,publish_stream"
+      self.oauth_authorize_url = "https://graph.facebook.com/oauth/authorize?client_id=#{CLIENT_ID}&redirect_uri=#{callback_url}&scope=manage_pages,offline_access,publish_stream"
       self.save!
     end
     self.oauth_authorize_url
   end
 
   def validate_oauth_token(oauth_verifier, callback_url = '')
-    response = RestClient.get("https://graph.facebook.com/oauth/access_token?client_id=#{CLIENT_ID}&redirect_uri=#{callback_url.html_safe}&client_secret=#{FACEBOOK_CLIENT_SECRET}&code=#{oauth_verifier.html_safe}")
+    response = RestClient.get("https://graph.facebook.com/oauth/access_token?client_id=#{CLIENT_ID}&redirect_uri=#{callback_url.html_safe}&client_secret=#{CLIENT_SECRET}&code=#{oauth_verifier.html_safe}")
     pair = response.body.split("&")[0].split("=")
     if (pair[0] == "access_token")
       self.access_token = pair[1]
@@ -26,7 +26,16 @@ class FacebookAccount < ActiveRecord::Base
   end
 
   def post(message)
-    RestClient.post 'https://graph.facebook.com/me/feed', { :access_token => self.access_token, :message => message }
+    RestClient.post "https://graph.facebook.com/#{self.page_id}/feed", { :access_token => self.page_token, :message => message }
+  end
+
+  def get_pages
+    response = RestClient.get(URI.encode("https://graph.facebook.com/me/accounts?access_token=#{self.access_token}"))
+    return JSON.parse(response.body)["data"]
+  end
+
+  def assign_page(page_id, page_token)
+    self.update_attributes(:page_id => page_id, :page_token => page_token)
   end
 
 end
