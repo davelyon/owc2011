e87e728860ed2db203782c8b46522a025414d152
Hashrocket Workstation <dev@hashrocket.com>
2011-08-21 06:05:34 -0500

    Delete and edit for quiz questions

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,20 +1,46 @@
 class Admin::QuestionsController < AdminController
+
+  before_filter :find_quiz
+
   def index
-    @quiz = Quiz.find(params[:quiz_id])
     @questions = @quiz.questions
   end
 
   def new
-    @question = Question.new(quiz_id: params[:quiz_id])
+    @question = Question.new
   end
 
   def create
     @question = Question.new params[:question]
-
+    @question.quiz = @quiz
     if @question.save
-      redirect_to admin_quiz_questions_path(@question.quiz), notice: "Question successfully created."
+      redirect_to admin_quiz_questions_path(@quiz), notice: "Question successfully created."
     else
       render :new
     end
+  end
+
+  def update
+    @question = @quiz.questions.find params[:id]
+    if @question.update_attributes params[:question]
+      redirect_to admin_quiz_questions_path(@quiz), notice: "Question successfully created."
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    @question = @quiz.questions.find params[:id]
+  end
+
+  def destroy
+    @quiz.questions.destroy params[:id]
+    redirect_to admin_quiz_questions_path(@quiz), notice: "Question successfully deleted."
+  end
+
+  private
+
+  def find_quiz
+    @quiz = Quiz.find(params[:quiz_id])
   end
 end
--- features/paypal_donations.feature
+++ features/paypal_donations.feature
@@ -6,15 +6,13 @@ Feature: PayPal donations
   Scenario: View the link to PayPal
     Given I am on the donations page
     Then I should see a PayPal button
-    
+
     #return variable
   Scenario: User Specified Donation Completed
     When I am on the donations page
     Then the PayPal button should set the return to the successful donation page
     When I am on the successful donations page
-    Then I should see "Thank you for supporting Kids Have Hope"  
-  
+    Then I should see "Thank you for supporting Kids Have Hope"
+
   # cancel_return variable
   Scenario: Donation Canceled
-  
-  
