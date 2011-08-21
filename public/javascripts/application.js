d911608cf9e440626ea14ea83806cc6b3d8caff2
Brian and David <brian@8thlight.com>
2011-08-21 03:55:55 -0500

    modalize that contact form

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,18 +1,17 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-
 function update_character() {
   $("#post_tweet_twitter_post").keyup(function() {
     var current = $("#post_tweet_twitter_post").attr("value");
-    var number_left =  140 - current.length;    
+    var number_left =  140 - current.length;
     $("#twitter_character_coundown").html(number_left);
   });
 };
 
-
-$(document).ready(update_character);
-
-$(document).ready(function(){
-  $(".datepicker").datepicker({ dateFormat: 'yy-mm-dd'});
-  
+$(document).ready(function() {
+  $(".datepicker").datepicker({dateFormat: 'yy-mm-dd'});
+  $('div.newsletter').dialog({autoOpen: false, width: 330, height: 135});
+  $('a.newsletter').live('click', function(event) {
+    event.preventDefault();
+    $('div.newsletter').dialog("open");
+  });
+  update_character;
 });
