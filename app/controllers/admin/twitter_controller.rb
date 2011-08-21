a7cc4c3d2ae72a3925fdec1775121bd98fabad46
Paul Pagel <paul@8thlight.com>
2011-08-20 20:47:42 -0500

    twitter working

--- app/controllers/admin/twitter_controller.rb
+++ app/controllers/admin/twitter_controller.rb
@@ -5,16 +5,17 @@ class Admin::TwitterController < ApplicationController
   
   def post_tweet
     Twitter.configure do |config|
-      config.consumer_key = "ABC"
-      config.consumer_secret =  "ABC"
-      config.oauth_token =  "ABC"
-      config.oauth_token_secret =  "ABC"
+      config.consumer_key = CONFIG[:twitter_consumer_key]
+      config.consumer_secret =  CONFIG[:twitter_consumer_secret]
+
+      config.oauth_token =  CONFIG[:twitter_oauth_token]
+      config.oauth_token_secret =  CONFIG[:twitter_oauth_token_secret]
     end
     
-    Twitter.update("I'm tweeting with @gem!")
+    Twitter.update(params[:post_tweet][:twitter_post])
     
     redirect_to admin_path
   rescue => e
-    render "error"
+    render "error"    
   end
 end
